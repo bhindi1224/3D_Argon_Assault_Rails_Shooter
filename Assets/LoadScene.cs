08d43deaf2676695b673373af92fc4964e48b0f2
Unknown <jamie1224@gmail.com>
2018-08-02 02:07:04 -0400

    made delay to load next scene

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+	[SerializeField] private float delay = 3f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		print(Time.time);
+		if (Time.time > delay)
+		{
+			int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+			SceneManager.LoadScene(currentSceneIndex+1);
+		}
+		
+	}
+}
