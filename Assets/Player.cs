3131b931ebf18d70b34ea3987bf22131906f613c
James Poore <jamie1224@gmail.com>
2018-08-03 02:13:17 -0400

    87 Understanding Roll, Pitch & Yaw

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In m/s")][SerializeField] float xSpeed = 4f;
-    [SerializeField] float xRestriction = 5f;
+    [Tooltip("In m/s")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m/s")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("In m/s")] [SerializeField] float xLimit = 5f;
+    [Tooltip("In m/s")] [SerializeField] float yUpperLimit = 5f;
+    [Tooltip("In m/s")] [SerializeField] float yLowerLimit = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +18,27 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        UpdateXPosition();
+        UpdateYPosition();
+    }
+
+    private void UpdateYPosition()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float NewYPos = Mathf.Clamp(rawNewYPos, yLowerLimit, yUpperLimit);
+        transform.localPosition = new Vector3(transform.localPosition.x, NewYPos, transform.localPosition.z);
+    }
+
+    private void UpdateXPosition()
+    {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawNewXPos = transform.localPosition.x + xOffset;
-        float NewXPos = Mathf.Clamp(rawNewXPos, -xRestriction, xRestriction);
+        float NewXPos = Mathf.Clamp(rawNewXPos, -xLimit, xLimit);
         transform.localPosition = new Vector3(NewXPos, transform.localPosition.y, transform.localPosition.z);
-	}
+    }
 }
