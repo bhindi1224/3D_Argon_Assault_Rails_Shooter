9d667a5dcdd4cb6d61b5e54649ba61d22da5faba
James Poore <jamie1224@gmail.com>
2018-08-04 00:11:07 -0400

    95 Project tidying checklist

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Use this for initialization
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
